eaabc731885fc89ea6ac6a85b6733fb7c33c94d0
2020-DEV-008 <2020.dev.008@gmail.com>
2019-11-25 03:16:27 +0530

    Scenario covered for all rolls are spare with additional roll is 5

--- src/test/java/com/bnpp/kata/BowlingGameTest.java
+++ src/test/java/com/bnpp/kata/BowlingGameTest.java
@@ -85,6 +85,15 @@ public class BowlingGameTest {
 		assertEquals(16, gameScore);
 	}
 
+	@Test
+	public void allRollsAreSpare() {
+		rollMany(21, 5);
+
+		int gameScore = game.calculateGameScore();
+
+		assertEquals(150, gameScore);
+	}
+
 	private void rollMany(final int numberOfRolls, final int numberOfPinsKnocked) {
 		for (int rollCount = 0; rollCount < numberOfRolls; rollCount++) {
 			game.roll(numberOfPinsKnocked);
