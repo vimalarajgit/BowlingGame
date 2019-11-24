4046920b0d7f6875c07284864591f208cc8aae8f
2020-DEV-008 <2020.dev.008@gmail.com>
2019-11-24 23:55:15 +0530

    Refactor: Extracted method to create instance for BowlingGame class

--- src/test/java/com/bnpp/kata/BowlingGameTest.java
+++ src/test/java/com/bnpp/kata/BowlingGameTest.java
@@ -3,20 +3,25 @@ package com.bnpp.kata;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 
+import org.junit.Before;
 import org.junit.Test;
 
 public class BowlingGameTest {
 
+	BowlingGame game;
+
+	@Before
+	public void init() {
+		game = new BowlingGame();
+	}
+
 	@Test
 	public void newBowlingGameInstanceShouldNotBeNull() {
-		BowlingGame game = new BowlingGame();
-
 		assertNotNull(game);
 	}
 
 	@Test
 	public void gameScoreShouldBeZeroBeforeGameStarts() {
-		BowlingGame game = new BowlingGame();
 		int gameScore = game.getGameScore();
 
 		assertEquals(0, gameScore);
